b5b5c00b90ee0fd9dc67bd2cfba52ac8d3940aa0
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2020-08-16 21:37:27 +0900

    done: section4

--- src/Dollar.ts
+++ src/Dollar.ts
@@ -1,9 +1,13 @@
 export class Dollar {
-  amount: number;
+  private amount: number;
   constructor(amount: number) {
     this.amount = amount;
   }
   times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
+  }
+  equals(object: Dollar): boolean {
+    const dollar = object;
+    return this.amount === dollar.amount;
   }
 }
--- test/money.test.ts
+++ test/money.test.ts
@@ -1,9 +1,12 @@
 import { Dollar } from "../src/Dollar";
 
-test("test", () => {
+test("multiplication", () => {
   const five: Dollar = new Dollar(5);
-  let product: Dollar = five.times(2);
-  expect(product.amount).toBe(10);
-  product = five.times(3);
-  expect(product.amount).toBe(15);
+  expect(five.times(2)).toStrictEqual(new Dollar(10));
+  expect(five.times(3)).toStrictEqual(new Dollar(15));
+});
+
+test("equality", () => {
+  expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+  expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
 });
